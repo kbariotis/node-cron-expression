1be45d5af1bbd29b723a0bff73d78eab950707e7
kbariotis <konmpar@gmail.com>
2022-01-03 00:00:01 +0200

    refactor between API

--- src/lib/between.spec.ts
+++ src/lib/between.spec.ts
@@ -1,5 +1,7 @@
 import { at } from './at';
 import { between } from './between';
+import { inMonth } from './inMonth';
+import { on } from './on';
 
 describe('between', () => {
   it('should provide minutes range', () => {
@@ -12,15 +14,19 @@ describe('between', () => {
     expect(between(at.hour(2), at.hour(11)).toString()).toBe('* 2-11 * * *');
   });
 
+  it('should provide month range', () => {
+    expect(between(inMonth(2), inMonth(11)).toString()).toBe('* * * 2-11 *');
+  });
+
   it('should provide days of the week, monday to friday', () => {
-    expect(between(at.dayOfTheWeek(0), at.dayOfTheWeek(4)).toString()).toBe(
+    expect(between(on.dayOfTheWeek(0), on.dayOfTheWeek(4)).toString()).toBe(
       '* * * * 0-4'
     );
   });
 
   it('should throw an error if different types', () => {
     expect(() =>
-      between(at.dayOfTheWeek(0), at.dayOfTheMonth(4)).toString()
+      between(on.dayOfTheWeek(0), on.dayOfTheMonth(4)).toString()
     ).toThrow();
   });
 });
